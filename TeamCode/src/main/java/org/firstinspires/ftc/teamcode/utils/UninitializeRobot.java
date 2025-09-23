b09c70946b36eb71a235213a0667eb9ca2bd9648
kjljixx <kjljixx@gmail.com>
2025-09-22 23:28:36 -0400

    Create UninitializeRobot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/UninitializeRobot.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name="Uninitialize Robot", group="Utils")
+public class UninitializeRobot extends OpMode {
+    @Override
+    public void init() {
+        Robot.initialized = false;
+        telemetry.addLine("Robot successfully uninitialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Robot successfully uninitialized; you may stop this OpMode");
+        telemetry.update();
+    }
+}
