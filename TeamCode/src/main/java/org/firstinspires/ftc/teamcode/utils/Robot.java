b89e864774fa5eee94ef5e702d6e05a77c5b6a91
kjljixx <kjljixx@gmail.com>
2025-09-26 22:40:29 -0400

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -4,10 +4,15 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.utils.rr.MecanumDrive;
+
+import java.util.List;
 
 //Allow configuration variables to be tuned without pushing code
 //with FTC Dashboard (https://acmerobotics.github.io/ftc-dashboard/features#configuration-variables)
@@ -18,6 +23,8 @@ public class Robot{
   //Mechanisms, IMU, etc.
   public static MecanumDrive drive;
   public static MultipleTelemetry telemetry;
+  public static Limelight3A limelight;
+  public static int obeliskAprilTagID; // -1 means no tag detected
   public static boolean initialized = false;
 
   public static void initialize(HardwareMap hardwareMap, Telemetry dsTelemetry){
@@ -26,7 +33,23 @@ public class Robot{
             dsTelemetry, // Driver Station telemetry
             FtcDashboard.getInstance().getTelemetry() // Dashboard telemetry
     );
+    limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    limelight.pipelineSwitch(4); // Obelisk AprilTag pipeline
+    limelight.start();
+    obeliskAprilTagID = -1;
 
     initialized = true;
+  }
+
+  public static void initializeLoop(){
+    LLResult result = limelight.getLatestResult();
+    if(result.isValid()){
+      List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+      for (LLResultTypes.FiducialResult fr : fiducialResults) {
+        obeliskAprilTagID = fr.getFiducialId();
+      }
+      telemetry.addData("Obelisk AprilTag ID: ", obeliskAprilTagID);
+      telemetry.update();
+    }
   }
 }
