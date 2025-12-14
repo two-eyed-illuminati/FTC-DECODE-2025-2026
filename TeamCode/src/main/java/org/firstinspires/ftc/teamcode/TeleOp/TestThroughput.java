85a99e74c46fbf3e8badd864c230dce162573cf9
kjljixx <kjljixx@gmail.com>
2025-12-14 11:34:51 -0500

    test throughput

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestThroughput.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.utils.Robot;
+
+@TeleOp(name="Test Throughput", group="Tests")
+public class TestThroughput extends OpMode {
+    int mode = 0;
+    @Override
+    public void init(){
+        Robot.initialize(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop(){
+       double power = gamepad1.left_stick_y;
+       Robot.intake.setPower(power);
+       Robot.transfer.setPower(0.4*power);
+       Robot.outtake.setPower(1.0);
+    }
+}
