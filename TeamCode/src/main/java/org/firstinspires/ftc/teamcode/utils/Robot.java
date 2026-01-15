7f68e0e9b94e4a92f90a2e4428a91dd4ead85de5
kjljixx <kjljixx@gmail.com>
2026-01-15 17:13:58 -0500

    try this pid thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -67,7 +67,7 @@ public class Robot{
       outtake = new ContinuousMotorMechanism(outtakeMotor,
               360.0/28.0, 36000.0
       );
-      outtakeController = new PIDFController(1.0/8000.0, 1.0/30345.0);
+      outtakeController = new PIDFController(1.0/4000.0, 1.0/30345.0);
       limelight = hardwareMap.get(Limelight3A.class, "limelight");
       limelight.pipelineSwitch(5);
       limelight.start();
