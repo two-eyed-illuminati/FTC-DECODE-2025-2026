701a8dfe3f0b342c6122712f3e868dff11fe497a
kjljixx <kjljixx@gmail.com>
2026-01-16 21:10:17 -0500

    ok its 21.39s but i added in realistic times for the shooting (3.5s)

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepCurrent.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepCurrent.java
@@ -1,25 +1,24 @@
 package com.example.meepmeep;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepSpline {
+public class MeepMeepCurrent {
     // --- Copy of your constants ---
     public static double START_X = -58.0586;
     public static double START_Y = -40.7964;
     public static double START_HEADING = -128.71;
-    public static double PRELOAD_SHOOT_X = -45.3370432609;
-    public static double PRELOAD_SHOOT_Y = -24.9996985274;
-    public static double PRELOAD_SHOOT_HEADING = -128.71;
-    public static double SPIKE_SHOOT_X = -20.3370432609;
-    public static double SPIKE_SHOOT_Y = -24.9996985274;
-    public static double SPIKE_SHOOT_HEADING = -90;
+    public static double SHOOT_X = -41.0621;
+    public static double SHOOT_Y = -29.6497;
+    public static double SHOOT_HEADING = -131.9655;
     public static double SPIKE_START_Y = -22.1017;
     public static double SPIKE_RAMP_END_Y = -49.1282;
     public static double SPIKE_TUNNEL_END_Y = -53.1282;
@@ -31,38 +30,28 @@ public class MeepMeepSpline {
 
     // --- Adapted helper methods (Removed Robot hardware calls) ---
     static TrajectoryActionBuilder trajToShoot(TrajectoryActionBuilder builder, boolean preload) {
-        if(preload){
-            Pose2d endRobotPose = new Pose2d(PRELOAD_SHOOT_X, PRELOAD_SHOOT_Y, Math.toRadians(PRELOAD_SHOOT_HEADING));
-            return builder.strafeToConstantHeading(
-                    endRobotPose.position
-            );
-        }
-        else{
-            Pose2d endRobotPose = new Pose2d(SPIKE_SHOOT_X, SPIKE_SHOOT_Y, Math.toRadians(SPIKE_SHOOT_HEADING));
-            return builder.splineToSplineHeading(
-                    endRobotPose,
-                    Math.toRadians(-190)
-            );
-        }
+        Pose2d endRobotPose = new Pose2d(SHOOT_X, SHOOT_Y, Math.toRadians(SHOOT_HEADING));
+        return builder.splineToSplineHeading(
+                endRobotPose,
+                Math.toRadians(preload ? 180 - SHOOT_HEADING : SHOOT_HEADING)
+        );
     }
 
     static TrajectoryActionBuilder intakeFromSpike(TrajectoryActionBuilder builder, int spike) {
         double currSpikeX = spike == 1 ? SPIKE_1_X : (spike == 2 ? SPIKE_2_X : SPIKE_3_X);
         double endSpikeX = spike == 2 ? SPIKE_2_END_X : currSpikeX;
-        double endSpikeY = spike <= 1 ? SPIKE_RAMP_END_Y : SPIKE_TUNNEL_END_Y;
-        return builder.splineToSplineHeading(
-                        new Pose2d(endSpikeX, endSpikeY, Math.toRadians(SPIKE_HEADING)),
-                        Math.toRadians(-SPIKE_HEADING),
+        return builder.strafeTo(
+                        new Vector2d(endSpikeX, spike <= 1 ? SPIKE_RAMP_END_Y : SPIKE_TUNNEL_END_Y),
                         new TranslationalVelConstraint(25.0)
                 )
                 .splineToSplineHeading(
-                        new Pose2d(endSpikeX, endSpikeY+5, Math.toRadians((SPIKE_HEADING*4 + SPIKE_SHOOT_HEADING) / 5.0)),
+                        new Pose2d(endSpikeX, SPIKE_START_Y, Math.toRadians((SPIKE_HEADING + SHOOT_HEADING) / 2.0)),
                         Math.toRadians(-SPIKE_HEADING)
                 );
     }
 
     public static void main(String[] args) {
-        com.noahbres.meepmeep.MeepMeep meepMeep = new com.noahbres.meepmeep.MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Robot constraints
@@ -78,7 +67,10 @@ public class MeepMeepSpline {
 
         // 2. To Spike 1
         TrajectoryActionBuilder toSpike1Tab = preloadShootTab.fresh()
-                .setTangent(Math.toRadians(20))
+                .splineToSplineHeading(
+                        new Pose2d(SHOOT_X, SHOOT_Y + 10.0, Math.toRadians((SPIKE_HEADING + SHOOT_HEADING) / 2.0)),
+                        Math.toRadians(0)
+                )
                 .splineToSplineHeading(
                         new Pose2d(SPIKE_1_X, SPIKE_START_Y, Math.toRadians(SPIKE_HEADING)),
                         Math.toRadians(SPIKE_HEADING)
@@ -88,7 +80,6 @@ public class MeepMeepSpline {
 
         // 3. To Spike 2
         TrajectoryActionBuilder toSpike2Tab = toSpike1IntakeAndShootTab.fresh()
-                .setTangent(Math.toRadians(0))
                 .splineToSplineHeading(
                         new Pose2d(SPIKE_2_X, SPIKE_START_Y, Math.toRadians(SPIKE_HEADING)),
                         Math.toRadians(SPIKE_HEADING)
@@ -97,9 +88,8 @@ public class MeepMeepSpline {
         TrajectoryActionBuilder toSpike2IntakeAndShootTab = trajToShoot(intakeFromSpike(toSpike2Tab, 2), false);
 
         // 4. Leave
-        TrajectoryActionBuilder leaveLaunchZoneTab = toSpike2IntakeAndShootTab.fresh().strafeToLinearHeading(
-                new Vector2d(SPIKE_SHOOT_X + 25, SPIKE_SHOOT_Y),
-                0
+        TrajectoryActionBuilder leaveLaunchZoneTab = toSpike2IntakeAndShootTab.fresh().strafeTo(
+                new Vector2d(SHOOT_X + 25, SHOOT_Y)
         );
 
         // Run it all in sequence
