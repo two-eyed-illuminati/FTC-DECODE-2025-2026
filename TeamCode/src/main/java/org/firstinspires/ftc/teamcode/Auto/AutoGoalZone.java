4f077c1ca8703a9b1692a04ff08fe07d119d709b
kjljixx <kjljixx@gmail.com>
2026-01-25 10:30:43 -0500

    surely???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoGoalZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoGoalZone.java
@@ -48,12 +48,18 @@ public class AutoGoalZone extends LinearOpMode {
     static TrajectoryActionBuilder trajToShoot(TrajectoryActionBuilder builder, int spike) {
         if (spike == 0) {
             Pose2d endRobotPose = new Pose2d(PRELOAD_SHOOT_X, PRELOAD_SHOOT_Y, Math.toRadians(PRELOAD_SHOOT_HEADING));
-            return builder.afterDisp(0, new Robot.PrepareShootAction(endRobotPose)).strafeToConstantHeading(endRobotPose.position);
+            return builder.afterDisp(0, () -> {
+                Robot.aimOuttakeTurret(endRobotPose, false);
+                Robot.shootOuttake(endRobotPose, false);
+            }).strafeToConstantHeading(endRobotPose.position);
         } else {
             double shootX = spike == 1 ? SPIKE_1_SHOOT_X : (spike == 2 ? SPIKE_2_SHOOT_X : SPIKE_3_SHOOT_X);
             double shootY = spike == 1 ? SPIKE_1_SHOOT_Y : (spike == 2 ? SPIKE_2_SHOOT_Y : SPIKE_3_SHOOT_Y);
             Pose2d endRobotPose = new Pose2d(shootX, shootY, Math.toRadians(SPIKE_SHOOT_HEADING));
-            return builder.afterDisp(0, new Robot.PrepareShootAction(endRobotPose)).splineToSplineHeading(
+            return builder.afterDisp(0, () -> {
+                Robot.aimOuttakeTurret(endRobotPose, false);
+                Robot.shootOuttake(endRobotPose, false);
+            }).splineToSplineHeading(
                     endRobotPose,
                     Math.toRadians(-190)
             );
