88ca76921a48b1da603c4fe05f6680cf3ed61250
kjljixx <kjljixx@gmail.com>
2026-01-27 19:22:55 -0500

    update

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepCurrent.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepCurrent.java
@@ -21,21 +21,21 @@ public class MeepMeepCurrent {
     public static double START_HEADING = -126.5;
     public static double PRELOAD_SHOOT_X = -45.3370432609;
     public static double PRELOAD_SHOOT_Y = -24.9996985274;
-    public static double PRELOAD_SHOOT_HEADING = -128.71;
+    public static double PRELOAD_SHOOT_HEADING = -126.5;
     public static double SPIKE_SHOOT_HEADING = -90;
     public static double SPIKE_START_Y = -24.1017;
     public static double SPIKE_RAMP_END_Y = -45.1282;
     public static double SPIKE_TUNNEL_END_Y = -51.1282;
     public static double SPIKE_HEADING = -90.0;
     public static double SPIKE_1_X = -12.3457;
-    public static double GATE_X = -7.0;
-    public static double GATE_Y = -54.0;
+    public static double GATE_X = -5.0;
+    public static double GATE_Y = -55.0;
     public static double SPIKE_1_SHOOT_X = -20.3370432609;
-    public static double SPIKE_1_SHOOT_Y = -33.9996985274;
-    public static double SPIKE_2_SHOOT_X = -12.3370432609;
-    public static double SPIKE_2_SHOOT_Y = -16.9996985274;
-    public static double SPIKE_3_SHOOT_X = -4.6629567391;
-    public static double SPIKE_3_SHOOT_Y = -8.9996985274;
+    public static double SPIKE_1_SHOOT_Y = -31.9996985274;
+    public static double SPIKE_2_SHOOT_X = -20.3370432609;
+    public static double SPIKE_2_SHOOT_Y = -31.9996985274;
+    public static double SPIKE_3_SHOOT_X = -20.3370432609;
+    public static double SPIKE_3_SHOOT_Y = -31.9996985274;
     public static double SPIKE_2_X = 12.3457;
     public static double SPIKE_2_END_X = 13.8457;
     public static double SPIKE_3_X = 34.3457;
@@ -63,23 +63,24 @@ public class MeepMeepCurrent {
         TrajectoryActionBuilder intake = builder.splineToSplineHeading(
                 new Pose2d(endSpikeX, endSpikeY, Math.toRadians(SPIKE_HEADING)),
                 Math.toRadians(-SPIKE_HEADING),
-                new TranslationalVelConstraint(15.0)
+                new TranslationalVelConstraint(20.0)
         );
         if(spike == 1){
             return intake.endTrajectory().splineToConstantHeading(
-                    new Vector2d(GATE_X, GATE_Y),
-                    Math.toRadians(SPIKE_HEADING)
-            ).strafeTo(new Vector2d(GATE_X, GATE_Y)).waitSeconds(0.5).splineToSplineHeading(
+                    new Vector2d(GATE_X, GATE_Y + 7.0),
+                    Math.toRadians(SPIKE_HEADING),
+                    new TranslationalVelConstraint(25.0)
+            ).strafeTo(
+                    new Vector2d(GATE_X, GATE_Y)
+            ).waitSeconds(0.2).splineToSplineHeading(
                     new Pose2d(GATE_X, GATE_Y + 5, Math.toRadians((SPIKE_HEADING * 4 + SPIKE_SHOOT_HEADING) / 5.0)),
-                    Math.toRadians(-SPIKE_HEADING),
-                    new TranslationalVelConstraint(15.0)
+                    Math.toRadians(-SPIKE_HEADING)
             );
         }
         else {
             return intake.splineToSplineHeading(
                     new Pose2d(endSpikeX, endSpikeY + 5, Math.toRadians((SPIKE_HEADING * 4 + SPIKE_SHOOT_HEADING) / 5.0)),
-                    Math.toRadians(-SPIKE_HEADING),
-                    new TranslationalVelConstraint(15.0)
+                    Math.toRadians(-SPIKE_HEADING)
             );
         }
     }
@@ -105,6 +106,11 @@ public class MeepMeepCurrent {
             }
         };
         TrajectoryActionBuilder toSpike1 = preloadShoot.fresh()
+//                .afterDisp(0, () -> {
+//                    Robot.intake.setPower(1.0);
+//                    Robot.transfer.setPos(0, 0.0*Robot.transfer.maxVel);
+//                    Robot.outtake.setPos(0, -1440.0);
+//                })
                 .setTangent(Math.toRadians(20))
                 .splineToSplineHeading(
                         new Pose2d(SPIKE_1_X, SPIKE_START_Y, Math.toRadians(SPIKE_HEADING)),
@@ -123,6 +129,11 @@ public class MeepMeepCurrent {
             }
         };
         TrajectoryActionBuilder toSpike2 = toSpike1IntakeAndShoot.fresh()
+//                .afterDisp(0, () -> {
+//                    Robot.intake.setPower(1.0);
+//                    Robot.transfer.setPos(0, 0.0*Robot.transfer.maxVel);
+//                    Robot.outtake.setPos(0, -1440.0);
+//                })
                 .setTangent(Math.toRadians(0))
                 .splineToSplineHeading(
                         new Pose2d(SPIKE_2_X, SPIKE_START_Y, Math.toRadians(SPIKE_HEADING)),
@@ -141,6 +152,11 @@ public class MeepMeepCurrent {
             }
         };
         TrajectoryActionBuilder toSpike3 = toSpike2IntakeAndShoot.fresh()
+//                .afterDisp(0, () -> {
+//                    Robot.intake.setPower(1.0);
+//                    Robot.transfer.setPos(0, 0.0*Robot.transfer.maxVel);
+//                    Robot.outtake.setPos(0, -1440.0);
+//                })
                 .setTangent(Math.toRadians(0))
                 .splineToSplineHeading(
                         new Pose2d(SPIKE_3_X, SPIKE_START_Y, Math.toRadians(SPIKE_HEADING)),
@@ -151,11 +167,6 @@ public class MeepMeepCurrent {
         TrajectoryActionBuilder toSpike3IntakeAndShoot = trajToShoot(intakeFromSpike(toSpike3, 3), 3);
 //        Action doSpike3Shoot = new Robot.ShootSequenceAction();
 
-        TrajectoryActionBuilder leaveLaunchZone = toSpike3IntakeAndShoot.fresh().strafeToLinearHeading(
-                new Vector2d(SPIKE_3_SHOOT_X, SPIKE_3_SHOOT_Y-20),
-                0
-        );
-
         myBot.runAction(new SequentialAction(
                 preloadShoot.build(),
                 new SleepAction(1.75),
@@ -164,8 +175,7 @@ public class MeepMeepCurrent {
                 toSpike2IntakeAndShoot.build(),
                 new SleepAction(1.75),
                 toSpike3IntakeAndShoot.build(),
-                new SleepAction(1.75),
-                leaveLaunchZone.build()
+                new SleepAction(1.75)
         ));
 
         meepMeep.setBackground(com.noahbres.meepmeep.MeepMeep.Background.FIELD_DECODE_JUICE_DARK) // You can change this to match your season
